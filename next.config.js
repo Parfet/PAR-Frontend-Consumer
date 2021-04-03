8cecd2905a4d6a599bd0133c8f81d5a32aef605c
First <natthasit.pan@innovasive.co.th>
2021-04-03 22:58:18 +0700

    👾 fixed css not load

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@ const withPlugins = require('next-compose-plugins')
 const withPWA = require('next-pwa')
 const withOffline = require('next-offline')
 
+const nextConfig = {
+  future: {
+    webpack5: true,
+    strictPostcssConfiguration: true
+  },
+};
+
 module.exports = withPlugins([
   [withOffline],
   [withPWA, {
@@ -9,4 +16,5 @@ module.exports = withPlugins([
       dest: 'public'
     },
   }]
-])
+], nextConfig)
+
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,20 @@
 import '../styles/tailwind.css'
 import 'dayjs/locale/th'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Meta from '../core/components/Meta'
 
 const MyApp = ({ Component, pageProps }) => {
-// 
+
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <>
       <Meta />
