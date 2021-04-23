28d7bffe59492655b16f4433c119c4a14013d1e1
First <natthasit.pan@innovasive.co.th>
2021-04-24 01:02:47 +0700

    👾 add PartyMember Modal

--- features/Party/pages/party.tsx
+++ features/Party/pages/party.tsx
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useRouter } from 'next/router'
 
 import { mockPartyMember } from '../../../core/config/mockData.js'
 import { RegularText } from '../../../core/config/textStyle'
-import PartyMember from '../components/PartyMember'
+import PartyMember from '../components/PartyPage/PartyMember'
+import MemberModal from '../components/PartyPage/MemberModal'
+
+type MemberDetail = {
+  memberId: string
+  imageURL: string
+  username: string
+  interestTag: string[]
+  rating: number
+}
 
 const Party = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const [memberDetail, setMemberDetail] = useState<MemberDetail>()
+  const [indexMember, setIndexMember] = useState(undefined)
   const router = useRouter()
+
+  const handleClickOpen = (memberDetail, index) => {
+    setIndexMember(index)
+    setMemberDetail(memberDetail)
+    setOpenModal(true)
+  };
+
+
+  const valueFromMemberModal = (value) => {
+    setOpenModal(value)
+  }
 
   const partyName = router.query
   return (
+    <>
     <div className="flex flex-col justify-center w-screen my-20 px-10">
       <div className="ml-2 mb-4">
         <RegularText bold className="text-gray-500">Party : {partyName.party}</RegularText>
@@ -19,6 +43,7 @@ const Party = () => {
           admin
           imageURL={mockPartyMember[0].imageURL}
           username={mockPartyMember[0].username}
+          onClick={() => handleClickOpen(mockPartyMember[0], undefined)}
           />
       </div>
       <div className="ml-2 my-4">
@@ -32,11 +57,24 @@ const Party = () => {
               username={data.username}
               key={index}
               keyId={index}
+              onClick={() => handleClickOpen(data, index)}
               />
-          ))
+              ))
         }
       </div>
     </div>
+    {
+      openModal ?
+        <MemberModal
+          showModal={openModal}
+          callBackToMemberParty={valueFromMemberModal}
+          memberDetail={memberDetail}
+          indexMember={indexMember}
+        />
+        : <></>
+    }
+    </>
+
   )
 }
 
