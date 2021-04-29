b194cc689c6a75124d0e40f3237ef72a8eea302f
First <natthasit.pan@innovasive.co.th>
2021-04-30 02:06:42 +0700

    👾 add type Restaurant

--- core/constant/type.ts
+++ core/constant/type.ts
@@ -15,6 +15,22 @@ export type Party = {
   archived_at? :string
 }
 
+export type Restaurant = {
+  restaurant_id :string
+  restaurant_name :string
+  email :string
+  tel_no :string
+  verify_status :boolean
+  status :string
+  rating :number
+  price :number
+  opened_time: string
+  closed_time: string
+  promotion :string[]
+  max_member :number
+  schedule_time :string
+}
+
 export type Member = {
   member_id: string
 }
