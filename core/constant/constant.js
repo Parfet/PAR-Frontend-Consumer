aeb9343c7628a9e05f7cc9b5443e60e781c49d69
First <natthasit.pan@innovasive.co.th>
2021-05-01 01:44:59 +0700

    👾 add constant ErrorMessage

--- core/constant/constant.js
+++ core/constant/constant.js
@@ -2,3 +2,9 @@ export const PartyTypeThai = {
   PUBLIC: "สาธารณะ",
   PRIVATE: "ส่วนตัว"
 }
+
+export const ErrorMessage = {
+  USER_NOT_FOUND: "ไม่พบผู้ใช้งาน",
+  PARTY_NOT_FOUND: "ไม่พบปาร์ตี้",
+  PASSCODE_INCORRECT: "รหัสผ่านไม่ถูกต้อง"
+}
