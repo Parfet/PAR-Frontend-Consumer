4b90fa3430b8d0437b9fff88bc7e7fdf476070fa
First <natthasit.pan@innovasive.co.th>
2021-04-30 23:48:37 +0700

    👾 delete apiParty in Core

--- core/service/apiParty.tsx
+++
@@ -1,9 +0,0 @@
-import api from '../../utils/api'
-
-export const apiParty = {
-  createParty: async (data) => {
-    console.log(data)
-    // const response = await api.post('/party', data)
-    // return response
-  },
-}
