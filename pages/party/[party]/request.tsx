5e8970766068baf2e87ee2c99bea34cbbcc855c7
First <natthasit.pan@innovasive.co.th>
2021-05-01 17:39:03 +0700

    👾 edit back router patj in pary request

--- pages/party/[party]/request.tsx
+++ pages/party/[party]/request.tsx
@@ -1,21 +1,19 @@
 import React, { useContext } from 'react'
-import { useRouter } from 'next/router'
 import { BottomNavigationAction, IconButton } from '@material-ui/core';
 import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
 
 import Navigator from '../../../core/components/Navigator'
 import PartyRequest from '../../../features/Party/pages/party-request'
+import { partyContext } from '../../../features/Party/contexts/party_context'
 
 const PartyPage = () => {
-  const router = useRouter()
-
-  const partyName = router.query
+  const contextParty = useContext(partyContext)
 
   return (
     <Navigator
       yellow
       backTextButton='Back'
-      backRoute={`/party/${partyName.party}`}
+      backRoute={`/party/${contextParty.currentParty.party_id}`}
       middleText="Request"
     >
       <PartyRequest />
