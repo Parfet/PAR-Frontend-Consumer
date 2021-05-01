c5471079b64d955b6f037e5fc162fc25e16ff50e
First <natthasit.pan@innovasive.co.th>
2021-05-01 17:38:30 +0700

    👾 edit use Component NoContent

--- features/Party/pages/party-list.tsx
+++ features/Party/pages/party-list.tsx
@@ -2,14 +2,12 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import styled from 'styled-components'
 import { useObserver } from 'mobx-react-lite'
-import { toJS } from 'mobx';
 import _ from "lodash"
-import CancelIcon from '@material-ui/icons/Cancel';
 
-import { SubTitle } from '../../../core/config/textStyle'
+import NoContent from '../../../core/components/NoContent'
+import { restaurantContext } from '../../Restaurant/contexts/restaurant_context'
 import CardParty from '../components/CardParty'
 import { partyContext } from '../contexts/party_context'
-import { restaurantContext } from '../../Restaurant/contexts/restaurant_context'
 
 const BackgroundPartyList = styled.div`
   background-color: #F8CE28;
@@ -29,14 +27,9 @@ const PartyList = () => {
     <BackgroundPartyList className="overscroll-auto pt-4 pb-10 mb-6">
       {
         _.size(contextParty.parties) === 0 ? 
-          <div className="flex justify-center flex-col w-full h-full">
-            <div className="flex justify-center mb-4">
-              <CancelIcon style={{ color: 'white', fontSize:'64px' }} />
-            </div>
-            <SubTitle className="self-center mb-12" white bold>
-              ไม่มี Party ในร้านอาหารนี้
-            </SubTitle>
-          </div>
+        <div className="flex justify-center flex-col w-full h-full">
+          <NoContent text="ไม่มี Party ในร้านอาหารนี้" white />
+        </div>
         :
         _.map(contextParty.parties, (data) => (
           <CardParty party={data} />
