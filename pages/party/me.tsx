99af37690f5e978f493a71473bb06d6785741e7b
First <natthasit.pan@innovasive.co.th>
2021-05-06 22:28:50 +0700

    👾 add floatingButton in MyParty

--- pages/party/me.tsx
+++ pages/party/me.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import styled from 'styled-components'
 import { IconButton } from '@material-ui/core';
 import SortIcon from '@material-ui/icons/Sort';
+import Fab from '@material-ui/core/Fab'
+import ListIcon from '@material-ui/icons/List';
 
 import Navigator from '../../core/components/Navigator'
 import MyParty from '../../features/Party/pages/my-party'
+
+const OverflowFab = styled(Fab)`
+  position: relative;
+  right:0;
+  background-color: white;
+`
 
 const CreatePartyPage = () => {
   const router = useRouter()
@@ -18,6 +27,11 @@ const CreatePartyPage = () => {
         <IconButton>
           <SortIcon />
         </IconButton>
+      }
+      floatingButton={
+        <OverflowFab >
+          <ListIcon />
+        </OverflowFab>
       }
     >
       <MyParty />
