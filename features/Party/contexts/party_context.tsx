6bb8b28c0604249f501ec50386d1b9ad2930adb1
First <natthasit.pan@innovasive.co.th>
2021-05-22 16:45:36 +0700

    👾 edit delete unused import

--- features/Party/contexts/party_context.tsx
+++ features/Party/contexts/party_context.tsx
@@ -1,4 +1,4 @@
-import { makeAutoObservable, runInAction } from 'mobx'
+import { makeAutoObservable } from 'mobx'
 import { createContext } from 'react'
 import { StatusCodes } from 'http-status-codes';
 
