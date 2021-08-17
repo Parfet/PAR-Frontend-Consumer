65f3025fb5d5a92f81865f446cd5099b02b79dc1
First-unknow <firstpan555@gmail.com>
2021-08-17 21:56:43 +0700

    👾 Fixed button onHover

--- features/Login/components/SignInButton.tsx
+++ features/Login/components/SignInButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import styled from 'styled-components'
 import { TextField, Button } from '@material-ui/core'
@@ -7,9 +7,9 @@ import { NormalText } from '../../../core/config/textStyle'
 
 const ButtonWithColor = styled(Button)`
   background-color: ${ (props) => props.bgColor ? props.bgColor : "" };
-  color: ${ (props) => props.bgColor ? "white" : "black" };
   border: ${ (props) => props.bgColor ? "" : "1px solid black" };
 `
+
 interface Props {
   text?: string
   image: string
@@ -18,10 +18,22 @@ interface Props {
 }
 
 const SignInButton = (props: Props) => {
+  const [textBlack, setTextBlack] = useState(true)
   const { text, image, onClick, bgColor } = props
+
+  const handleMouseOver = () => {
+    setTextBlack(false);
+  }
+
+  const handleMouseOut = () => {
+    setTextBlack(true);
+  }
+
   return (
     <ButtonWithColor 
       onClick={onClick} 
+      onMouseOver={() => handleMouseOver()}
+      onMouseOut={() => handleMouseOut()}
       className="flex rounded-5 w-72 justify-start pl-4"
       bgColor={bgColor}
       >
@@ -31,13 +43,12 @@ const SignInButton = (props: Props) => {
         width={"32px"}
         height={"32px"}
       />
-      <div className="ml-4">
-        <NormalText
-          white={bgColor ? "white" : ""}
-        >
-          {text}
-        </NormalText>
-      </div>
+      <NormalText
+        className="ml-4"
+        white={bgColor ? textBlack : false}
+      >
+        {text}
+      </NormalText>
     </ButtonWithColor>
   )
 }
