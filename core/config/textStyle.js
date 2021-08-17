0cd85ebb8ca5cfca62b546176e81ac35c5bae9d8
First-unknow <firstpan555@gmail.com>
2021-08-17 21:57:50 +0700

    👾 Delete Hover at textStyled

--- core/config/textStyle.js
+++ core/config/textStyle.js
@@ -4,109 +4,45 @@ export const Title = styled.div`
   font-size: 24px;
   font-weight: bold;
   color: ${ props => props.white ? 'white' : 'black'};
-
-  &:hover{
-    color: ${ props => 'black'};
-  }
-  
-  &:active{
-    color: ${props => 'black' };
-  }
 `
 
 export const SubTitle = styled.div`
   font-size: 20px;
   font-weight: bold;
   color: ${ props => props.white ? 'white' : 'black'};
-
-  &:hover{
-    color: ${ props => 'black'};
-  }
-  
-  &:active{
-    color: ${props => 'black' };
-  }
 `
 
 export const Header = styled.div`
   font-size: 18px;
   font-weight: bold;
   color: ${ props => props.white ? 'white' : 'black'};
-
-  &:hover{
-    color: ${ props => 'black'};
-  }
-  
-  &:active{
-    color: ${props => 'black' };
-  }
 `
 
 export const SubHeader = styled.span`
   font-size: 16px;
   font-weight: ${props => props.bold ? 'bold' : 'normal'};
   color: ${ props => props.white ? 'white' : 'black'};
-
-  &:hover{
-    color: ${ props => 'black'};
-  }
-  
-  &:active{
-    color: ${props => 'black' };
-  }
 `
 
 export const RegularText = styled.span`
   font-size: 14px;
   font-weight: ${props => props.bold ? 'bold' : 'normal'};
   color: ${ props => props.white ? 'white' : 'black'};
-
-  &:hover{
-    color: ${ props => 'black'};
-  }
-  
-  &:active{
-    color: ${props => 'black' };
-  }
 `
 
 export const NormalText = styled.span`
   font-size: 12px;
   font-weight: ${props => props.bold ? 'bold' : 'normal'};
   color: ${ props => props.white ? 'white' : 'black'};
-
-  &:hover{
-    color: ${ props => 'black' };
-  }
-  
-  &:active{
-    color: ${ props => 'black' };
-  }
 `
 
 export const SmallText = styled.span`
   font-size: 10px;
   color: ${ props => props.white ? 'white' : 'black'};
-
-  &:hover{
-    color: ${ props => 'black'};
-  }
-  
-  &:active{
-    color: ${props => 'black' };
-  }
 `
 
 export const TinyText = styled.span`
   font-size: 9px;
   color: ${ props => props.white ? 'white' : 'black'};
-
-  &:hover{
-    color: ${ props => 'black'};
-  }
-  
-  &:active{
-    color: ${props => 'black' };
-  }
 `
 
