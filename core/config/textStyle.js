8137f9c1a6ba6c1cf2c5c41dcb948bde1e38a906
First-unknow <firstpan555@gmail.com>
2021-09-21 23:01:59 +0700

    👾 Update cut string if it's too long

--- core/config/textStyle.js
+++ core/config/textStyle.js
@@ -4,45 +4,85 @@ export const Title = styled.div`
   font-size: 24px;
   font-weight: bold;
   color: ${ props => props.white ? 'white' : 'black'};
+  text-overflow: ${ props => props.isCut ? 'ellipsis' : ''};;
+
+  /* Required for text-overflow to do anything */
+  white-space: ${ props => props.isCut ? 'nowrap' : ''};
+  overflow: ${props => props.isCut ? 'hidden' : ''};
 `
 
 export const SubTitle = styled.div`
   font-size: 20px;
   font-weight: bold;
   color: ${ props => props.white ? 'white' : 'black'};
+  text-overflow: ${ props => props.isCut ? 'ellipsis' : ''};
+
+  /* Required for text-overflow to do anything */
+  white-space: ${ props => props.isCut ? 'nowrap' : ''};
+  overflow: ${ props => props.isCut ? 'hidden' : ''};
 `
 
 export const Header = styled.div`
   font-size: 18px;
   font-weight: bold;
   color: ${ props => props.white ? 'white' : 'black'};
+  text-overflow: ${ props => props.isCut ? 'ellipsis' : ''};
+
+  /* Required for text-overflow to do anything */
+  white-space: ${ props => props.isCut ? 'nowrap' : ''};
+  overflow: ${ props => props.isCut ? 'hidden' : ''};
 `
 
 export const SubHeader = styled.span`
   font-size: 16px;
   font-weight: ${props => props.bold ? 'bold' : 'normal'};
   color: ${ props => props.white ? 'white' : 'black'};
+  text-overflow: ${ props => props.isCut ? 'ellipsis' : ''};
+
+  /* Required for text-overflow to do anything */
+  white-space: ${ props => props.isCut ? 'nowrap' : ''};
+  overflow: ${ props => props.isCut ? 'hidden' : ''};
 `
 
 export const RegularText = styled.span`
   font-size: 14px;
   font-weight: ${props => props.bold ? 'bold' : 'normal'};
   color: ${ props => props.white ? 'white' : 'black'};
+  text-overflow: ${ props => props.isCut ? 'ellipsis' : ''};
+
+  /* Required for text-overflow to do anything */
+  white-space: ${ props => props.isCut ? 'nowrap' : ''};
+  overflow: ${ props => props.isCut ? 'hidden' : ''};
 `
 
 export const NormalText = styled.span`
   font-size: 12px;
   font-weight: ${props => props.bold ? 'bold' : 'normal'};
   color: ${ props => props.white ? 'white' : 'black'};
+  text-overflow: ${ props => props.isCut ? 'ellipsis' : ''};
+
+  /* Required for text-overflow to do anything */
+  white-space: ${ props => props.isCut ? 'nowrap' : ''};
+  overflow: ${ props => props.isCut ? 'hidden' : ''};
 `
 
 export const SmallText = styled.span`
   font-size: 10px;
   color: ${ props => props.white ? 'white' : 'black'};
+  text-overflow: ${ props => props.isCut ? 'ellipsis' : ''};
+
+  /* Required for text-overflow to do anything */
+  white-space: ${ props => props.isCut ? 'nowrap' : ''};
+  overflow: ${ props => props.isCut ? 'hidden' : ''};
 `
 
 export const TinyText = styled.span`
   font-size: 9px;
   color: ${ props => props.white ? 'white' : 'black'};
+  text-overflow: ${ props => props.isCut ? 'ellipsis' : ''};
+
+  /* Required for text-overflow to do anything */
+  white-space: ${ props => props.isCut ? 'nowrap' : ''};
+  overflow: ${ props => props.isCut ? 'hidden' : ''};
 `
 
