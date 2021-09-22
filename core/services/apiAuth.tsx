2f397e747356927a6e9099896bf5f3e501ebb295
First-unknow <firstpan555@gmail.com>
2021-09-22 21:44:20 +0700

    👾 Update rename getUserData and remove getJWT

--- core/services/apiAuth.tsx
+++ core/services/apiAuth.tsx
@@ -1,12 +1,8 @@
 import api from '../../utils/api'
 
 const apiUser = {
-  getUser: async () => {
-    const response = await api.get('/user')
-    return response
-  },
-  getJWTToken: async (data) => {
-    const response = await api.post('/auth/token', data)
+  getUserData: async () => {
+    const response = await api.get('/user/me')
     return response
   },
   checkUser: async () => {
