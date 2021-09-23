bc806cc690d7521eaa8ac49b4093b2f281e191fa
First-unknow <firstpan555@gmail.com>
2021-09-23 15:12:37 +0700

    👾 Fixed service-worker multi loading

--- next.config.js
+++ next.config.js
@@ -26,7 +26,10 @@ module.exports = withPlugins([
   [withPWA, {
     pwa: {
       // disable: process.env.NODE_ENV === 'development',
-      dest: 'public'
+      dest: 'public',
+      register: true,
+      scope: '/app',
+      sw: 'service-worker.js',
     },
   }]
 ], nextConfig)
