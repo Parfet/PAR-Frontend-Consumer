005f170ee8d48e5b9430158f9d0f2a6832b8a41f
First-unknow <firstpan555@gmail.com>
2021-10-09 22:16:07 +0700

    Update condition before go into registerpages

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useEffect } from 'react'
+import router from 'next/router';
 
+import { useAuth } from '../core/config/auth';
 import Navigator from '../core/components/Navigator'
 import Register from '../features/Register/pages';
+import { useUser } from '../core/context/auth_context';
 
+const RegisterPages = () => {
+  const auth = useAuth();
+  const userContext = useUser();
 
-const SignInPages = () => {
-
+  useEffect(() => {
+    if (auth.user == null) {
+      router.replace('/signin')
+    } else if (userContext.userData){
+      router.replace('/')
+    }
+  }, [])
   return (
-    <Navigator yellow middleText='Register'>
+    <Navigator yellow centerAppBar middleText='Register'>
       <Register />
     </Navigator>
   )
 }
 
-export default SignInPages
+export default RegisterPages
