663952795ff7b9fce157e8d00962d3cecdd64b96
First-unknow <firstpan555@gmail.com>
2021-10-10 21:50:29 +0700

    Fixed back route in My Party

--- pages/party/me.tsx
+++ pages/party/me.tsx
@@ -37,7 +37,7 @@ const CreatePartyPage = () => {
   return (
     <Navigator
       backTextButton='Home'
-      backRoute={() => router.back()}
+      backRoute={() => router.push('/')}
       middleText='My Party'
       leftIcon={
         <IconButton>
