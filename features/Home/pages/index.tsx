62c1e324d2b23520afe4767ea0f64775ff1236e7
First-unknow <firstpan555@gmail.com>
2021-10-15 00:08:55 +0700

    Update Home page

--- features/Home/pages/index.tsx
+++ features/Home/pages/index.tsx
@@ -1,26 +1,76 @@
 import React, { useEffect, useContext, useState } from 'react'
 import { useRouter } from 'next/router'
 import styled from 'styled-components'
-import Image from 'next/image'
+import _ from 'lodash'
 import { Button } from '@material-ui/core'
 
 import { useUser } from '../../../core/context/auth_context';
-import { Title, SubHeader } from '../../../core/config/textStyle';
+import NoContent from '../../../core/components/NoContent'
+import { useParty } from '../../Party/contexts/party_context';
+import CardMyParty from '../../Party/components/CardMyParty';
+import { Title, SubHeader, NormalText } from '../../../core/config/textStyle';
 
 const Background = styled.div`
   background-color: #F8CE28;
+  height: 95vh;
 `
+
+const ButtonWithColor = styled(Button)`
+  background-color: ${(props: ButtonProps) => props.color ? props.color : "#BD470A" };
+`
+interface ButtonProps {
+  color?: string
+}
 
 const Home = () => {
   const userContext = useUser();
+  const partyContext = useParty();
   const router = useRouter()
 
-  return (
-    <Background className="pt-10 h-screen flex items-center justify-center flex-col space-y-10">
-      <Button variant="contained" color="primary" onClick={() => router.push('/restaurant')}> Choose restaurant </Button>
-      <Button variant="contained" color="primary" onClick={() => router.push('/party/me')}> My party</Button>
-      <Button variant="contained" color="primary" onClick={() => router.push('/party/history')}> History</Button>
-      <Button variant="contained" color="secondary" onClick={() => userContext.signout()}> sign out</Button>
+  useEffect(() => {
+    (async () => {
+      await partyContext.getPartyByUserId()
+    })()
+  }, [])
+
+  return  (
+    <Background className="pt-10 overflow-hidden">
+      <div className="flex justify-between px-8">
+        <SubHeader bold>ปาร์ตี้ของของคุณ</SubHeader>
+        <NormalText className="text-black pt-1 underline" onClick={() => router.push('/party/me')}> ดูทั้งหมด </NormalText>
+      </div>
+      {
+        _.size(partyContext.allMyParty) === 0 ?
+          <div className="flex justify-center flex-col w-full h-1/2">
+            <NoContent text="คุณไม่มี Party ที่เข้าร่วมในตอนนี้" white />
+          </div>
+          :
+          _.map(partyContext.allMyParty, (data, index) => (
+            <>
+              {
+                index < 2 ? 
+                <CardMyParty party={data} key={index} />
+                  : <></>
+              }
+            </>
+          ))
+      }
+      <div className="px-8 space-y-5">
+        <div className="flex">
+          <ButtonWithColor variant="contained" className="w-full text-white" onClick={() => router.push('/restaurant')}> เลือกร้านอาหาร </ButtonWithColor>
+          {
+            process.env.NODE_ENV == "development" ?
+              <div className="flex">
+                <Button variant="contained" color="secondary" onClick={() => userContext.signout()}> sign out</Button>
+              </div>
+              : <></>
+          }
+        </div>
+        <div className="flex justify-between space-x-5">
+          <ButtonWithColor variant="contained" className="w-1/2 text-white" onClick={() => router.push('/party/history')}> ประวัติ </ButtonWithColor>
+          <ButtonWithColor variant="contained" className="w-1/2 text-white" onClick={() => router.push('/party/request')}> รายการคำขอ </ButtonWithColor>
+        </div>
+      </div>
     </Background>
   )
 }
