b79a2ac2a2c58376a78159b486ede4a471fbbd59
First-unknow <firstpan555@gmail.com>
2021-11-15 23:18:07 +0700

    Add White Loading

--- core/components/Loading.tsx
+++ core/components/Loading.tsx
@@ -1,7 +1,13 @@
 import React, { useEffect, useState } from "react";
 import * as loadingData from "../config/animations/loading.json";
+import * as loadingWhite from "../config/animations/whiteLoading.json";
 import FadeIn from "react-fade-in";
 import Lottie from "react-lottie";
+
+interface LoadingProps {
+  isWhite?: boolean
+  isFull?: boolean
+}
 
 const defaultOptions = {
   loop: true,
@@ -12,14 +18,28 @@ const defaultOptions = {
   }
 };
 
-const Loading = () => {
+const whiteOption = {
+  loop: true,
+  autoplay: true,
+  animationData: loadingWhite,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice"
+  }
+};
+
+const Loading = (props: LoadingProps) => {
+  const { isWhite, isFull } = props
 
   return (
     <div className="App">
       <header className="App-header">
           <FadeIn>
-            <div className="flex h-screen items-center">
-              <Lottie options={defaultOptions} height={140} width={140} />
+          <div className={`flex h-screen ${isFull ? 'items-center' : 'items-start pt-40'}`}>
+              {
+                isWhite ?
+                <Lottie options={whiteOption} height={140} width={140} />
+                : <Lottie options={defaultOptions} height={140} width={140} />
+              }
             </div>
           </FadeIn>
       </header>
